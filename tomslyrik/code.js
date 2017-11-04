1c4997da9f747f23b4f21c9debeccd5d06db38ae
Tom Brückner <tom@guzz.io>
2017-11-04 19:55:42 +0100

    objekt

--- tomslyrik/code.js
+++ tomslyrik/code.js
@@ -1,15 +1,9 @@
-function zahlEingeben() {
-  var zahl = prompt();
-  var meineWurzel = wurzelZiehen(zahl);
-  console.log(meineWurzel);
-}
-
-function wurzelZiehen(zahl) {
-  var wurzel = Math.sqrt(zahl);
-  return wurzel;
-}
-
-for (var i = 0; i < 500; i++) {
-  var wurzel = wurzelZiehen(i);
-  console.log(`Wurzel von ${i}: ${wurzel}`);
-}
+var junge = {
+  name: 'Joona',
+  alter: 14,
+  schuhgroesse: 42,
+  ausgeben: function() {
+    console.log(this.name);
+  }
+};
+junge.ausgeben();
