af89927d71a4cc1245dce3c40564fdd14f4d4705
bluewingtitan <eingressplayer@gmail.com>
2017-11-18 20:20:12 +0100

    traumata

--- hmez/WEBSITE/rotate.js
+++ hmez/WEBSITE/rotate.js
@@ -2,7 +2,7 @@ var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
-  this.period = parseInt(period, 8) || 1000;
+  this.period = parseInt(period, 10) || 2000;
   this.txt = '';
   this.tick();
   this.isDeleting = false;
@@ -21,7 +21,7 @@ TxtRotate.prototype.tick = function() {
   this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
   var that = this;
-  var delta = 300 - Math.random() * 50;
+  var delta = 300 - Math.random() * 100;
 
   if (this.isDeleting) {
     delta /= 2;
@@ -33,7 +33,7 @@ TxtRotate.prototype.tick = function() {
   } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
-    delta = 250;
+    delta = 500;
   }
 
   setTimeout(function() {
@@ -53,6 +53,6 @@ window.onload = function() {
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #fff }";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
