e14c321c5ef19b8cace8a1ed587efe1fefc02e76
Zockedidock <joona.brueckner@gmail.com>
2017-11-18 19:18:18 +0100

    - Opacaty changed

--- Joona/joona.js
+++ Joona/joona.js
@@ -1,4 +1,5 @@
 function hallo() {
+  console.log(Ducument);
   var yearsold = prompt('Wie alt bist du');
   if (yearsold >= 16) {
     document.location.href = 'joona2.html';
@@ -7,4 +8,11 @@ function hallo() {
     document.location.href = 'joona.html';
     alert('Mit ' + yearsold + ' Jahren ist dir der Zugang auf meiner Website verweigert.');
   }
+}
+var opacity = 1;
+function vanish() {
+  var katze = document.getElementById('katze');
+  // katze.className = "hidden";
+    opacity = opacity - 0.1;
+    katze.style = "opacity: " + opacity;
 }
